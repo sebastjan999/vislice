d1a699c81d201460810f551864047a577891f510
Sebastjan Šenk <sebastjan.senk@student.fmf.uni-lj.si>
2019-04-24 15:24:06 +0200

    merge conflict !!!!

--- test.py
+++ test.py
@@ -9,8 +9,4 @@ def je_prastevilo(n):
 for x in range(2,201): 
     if je_prastevilo:
         print(x)   
-<<<<<<< HEAD
-        fuufuf
-=======
-        lol
->>>>>>> ac6fba6ef93c474e36678248ba1b9f26f50c335e
+        fuufuf2fhoi
