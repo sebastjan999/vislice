b119ec24d4f25c3396071eecbb876785eb1a1a78
Sebastjan Šenk <sebastjan.senk@student.fmf.uni-lj.si>
2019-04-24 15:11:57 +0200

    ovoono

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-#Napisi ptogram, ki izpise vsa prastevila manjsa od 200
+# Napisi program, ki izpise vsa prastevila manjsa od 200
 
 def je_prastevilo(n):
     for i in range(2,n):
         if n % i == 0:
-            return False
+            return Falsezifuf
     return True
 
 for x in range(2,201): 
     if je_prastevilo:
         print(x)   
+        fuufuf
