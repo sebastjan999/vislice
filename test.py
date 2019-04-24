dfee5d6eb8259290fb2ddcc758b6e5eb22c4abba
Sebastjan Šenk <sebastjan.senk@student.fmf.uni-lj.si>
2019-04-24 14:55:56 +0200

    sprememba testa

---
+++ test.py
@@ -0,0 +1,11 @@
+#Napisi ptogram, ki izpise vsa prastevila manjsa od 200
+
+def je_prastevilo(n):
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
+
+for x in range(2,201): 
+    if je_prastevilo:
+        print(x)   
