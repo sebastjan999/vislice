180a2802477fe9debaa045ecf9625068f1711f9c
sebastjan999 <49947113+sebastjan999@users.noreply.github.com>
2019-05-06 09:25:12 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
 # Napisi program, ki izpise vsa prastevila manjsa od 200
 
 def je_prastevilo(n):
-    for i in range(2,n):
+    for i in range(2,n**0.5):
         if n % i == 0:
-            return Falsezifuf
+            return False
     return True
 
 for x in range(2,201): 
-    if je_prastevilo:
+    if je_prastevilo(x):
         print(x)   
 
-        fuufuf2fhoi
-        fuufuf
-        lol
-
+        
